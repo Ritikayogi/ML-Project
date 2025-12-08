733f9828149b262848ca442247caad1a70140973
Ritikayogi <yogiritika02@gmail.com>
2025-12-09 04:35:25 +0530

    First commit

--- src/mlproject/pipeliness/prediction_pipeline.py
+++ src/mlproject/pipeliness/prediction_pipeline.py
@@ -1,20 +1,23 @@
+import os
 import sys
 import pandas as pd
 from src.mlproject.exception import CustomException
 from src.mlproject.utils import load_object
 
 class PredictPipeline:
-    def predict(self, features: pd.DataFrame):
+    def predict(self, features):
         try:
-            model_path = "artifacts/model.pkl"
-            preprocessor_path = "artifacts/preprocessor.pkl"
+            base_dir = os.path.dirname(os.path.dirname(os.path.dirname(_file_)))
+
+            model_path = os.path.join(base_dir, "artifacts", "model.pkl")
+            preprocessor_path = os.path.join(base_dir, "artifacts", "preprocessor.pkl")
 
             model = load_object(model_path)
             preprocessor = load_object(preprocessor_path)
 
             data_transformed = preprocessor.transform(features)
-
             prediction = model.predict(data_transformed)
+
             return prediction
 
         except Exception as e:
